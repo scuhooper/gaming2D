b98e1d06c2487eab51e09297afd98c321c0388ad
CMBthe42 <yroc.999@gmail.com>
2016-10-18 21:53:36 -0400

    no message

---
+++ Assets/Player.js
@@ -0,0 +1,12 @@
+﻿var speed : float = 1.0;
+var animator: Animator;
+ 
+ function Update() {
+     var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+     transform.position += move * speed * Time.deltaTime; 
+
+     if(Input.GetAxis("Vertical"))
+     {
+     	isWalkingDown = true;
+     }
+ }
