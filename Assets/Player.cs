632f9b61d95422c6f65ced59e489afe9c7fa5c4c
CMBthe42 <yroc.999@gmail.com>
2016-10-25 20:04:08 -0400

    updated to the rpg for class with animations

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+	public float speed = 10.0F;
+	public bool isWalking;
+
+	public float Speed = 0f;
+	private float movex = 0f;
+	private float movey = 0f;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		movex = Input.GetAxis("Horizontal");
+		movey = Input.GetAxis("Vertical");
+		GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
+
+
+		if ((movex >= 0.1f) || (movey >= 0.1f) || (movex <= -0.1f) || (movey <= -0.1f))
+		{
+			animator.SetBool("isWalking", true);
+		}
+		else
+			animator.SetBool("isWalking", false);
+	}
+}
