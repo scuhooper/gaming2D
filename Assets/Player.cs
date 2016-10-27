c96043d6db0ca4a8a96a1ada0ce7e876903e1f62
CMBthe42 <yroc.999@gmail.com>
2016-10-27 16:25:35 -0400

    no message

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,9 +5,8 @@ public class Player : MonoBehaviour
 {
 
 	public float speed = 10.0F;
-	public bool isWalking;
+	//public bool isWalking;
 	public GameObject hitBox;
-	public GameObject lefthitBox;
 	public int Speed = 0;
 	private float movex = 0f;
 	private float movey = 0f;
@@ -18,37 +17,55 @@ public class Player : MonoBehaviour
 	{
 		animator = GetComponent<Animator>();
 		hitBox.SetActive(false);
-		lefthitBox.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
+		//Movement
 		movex = Input.GetAxis("Horizontal");
 		movey = Input.GetAxis("Vertical");
 		GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
 
 
-		if ((movex >= 0.1f) || (movey >= 0.1f) || (movex <= -0.1f) || (movey <= -0.1f))
+		if ((movex >= 0.1f) || (movex <= -0.1f) || (movey <= -0.1f))
 		{
 			animator.SetBool("isWalking", true);
 		}
-		else
+		else {
 			animator.SetBool("isWalking", false);
+		}
+		if (movey <= -0.1f)
+		{
+			animator.SetBool("walkUp", true);
+		}
+		else {
+			animator.SetBool("walkUp", false);
+		}
+		if (movey >= 0.1f)
+		{
+			animator.SetBool("walkDown", true);
+		}
+		else {
+			animator.SetBool("walkDown", false);
+		}
 
+		//Attack
 		if (Input.GetMouseButtonDown(0))
 		{
 			hitBox.SetActive(true);
-			Debug.Log("PIE");
 		}
 		else {
 			hitBox.SetActive(false);
 		}
+
+		//flips the player left and right
 		if (movex <= -0.1f)
 		{
 			transform.rotation = Quaternion.Euler(0, 180, 0);
 		}
-		else {
+		if (movex >= 0.1f)
+		{
 			transform.rotation = Quaternion.Euler(0, 0, 0);
 		}
 	}
