de3b14bc77343c65a0a73fd4f05296da87c25f83
CMBthe42 <yroc.999@gmail.com>
2016-10-31 14:54:44 -0400

    working on spawner for joints

---
+++ Assets/jointSpawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jointSpawn : MonoBehaviour
+{
+	public GameObject go;
+	int currentObj = 0;
+	int maxObject = 10;
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine("SpawnJoint");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+	}
+
+	IEnumerator SpawnJoint()
+	{
+		if (currentObj < maxObject)
+			Instantiate(go, transform);
+		currentObj++;
+		return (null);
+	}
+}
