0cedde47ff5faa69618d1643e652dfc721d2fc8d
CMBthe42 <yroc.999@gmail.com>
2016-11-01 12:45:24 -0400

    working on a scene to test mechanics. placed pick-ups for Barry to pick up.

---
+++ Assets/joint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class joint : MonoBehaviour
+{
+
+	public void OnTrigger2DEnter(Collision2D col)
+	{
+
+		if (col.gameObject.name == "player")
+		{
+			GetComponent<SpriteRenderer>().enabled = false;
+		}
+	}
+
+}
