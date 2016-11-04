78a1933863f59e6863ac14599038c8c2f4fb0f9d
CMBthe42 <yroc.999@gmail.com>
2016-11-04 09:59:33 -0400

    minor changes

---
+++ Assets/Scripts/joint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class joint : MonoBehaviour
+{
+	//public GameObject jointGO;
+
+	public void Start()
+	{
+		DestroyJoint();
+	}
+
+	public void DestroyJoint()
+	{
+		Destroy(gameObject, 10);
+	}
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.name == "player" || col.gameObject.name == "joint(Clone)")
+		{
+			GetComponent<SpriteRenderer>().enabled = false;
+		}
+
+	}
+}
