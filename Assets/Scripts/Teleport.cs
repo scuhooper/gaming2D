3686e3d8f7c5f0cfaa2f81c63c04eb763dc4ad7b
scuhooper <keeling18@marshall.edu>
2016-11-29 15:15:46 -0500

    Teleporter now shows line in game
    Added a line from the player to the mouse that shows in game.
    This will need to be adjusted to have a max length decided later.
    Need to add a faded copy of barry to the end of the line, also need to
    check for collisions and adjust the ray accordingly.
    Ray should eventually only show when the ability is available and only
    show valid locations for barry to be placed.

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	LineRenderer line;
+
+	// Use this for initialization
+	void Start () {
+		line = GetComponent<LineRenderer>();
+		line.useWorldSpace = true;
+		// line.SetColors( Color.yellow, Color.yellow );
+		line.SetWidth( .1f, .1f );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+		mousePos.z = -10;
+		// Debug.Log( mousePos.ToString() );
+
+		line.SetPosition( 0, transform.position );
+		line.SetPosition( 1, mousePos );
+		line.enabled = true;
+		// Debug.DrawLine( transform.position, mousePos, Color.yellow, 0, false );
+	}
+}
