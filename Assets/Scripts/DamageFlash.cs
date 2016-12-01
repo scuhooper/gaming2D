7fe2b3be80a867cb7816d76fd0f0a193a1a0a3fd
Cameron Asbury <asbury56@marshall.edu>
2016-12-01 01:16:10 -0500

    Added HP, flash, and attack
    Added HP script, a flash image script, and an enemy attack script

--- Assets/Scripts/DamageFlash.cs
+++ Assets/Scripts/DamageFlash.cs
@@ -1,19 +1,33 @@
 ﻿/*********
  *		Author:Cameron Asbury 
- *		Purpose:Force Camera into a selected aspect ratio, so no matter what screen resolution a player picks on screen artifacts all remain the same. 
+ *		Purpose:Will Flash a different image on the screen when the Player is attacked
  ********/
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DamageFlash : MonoBehaviour {
 
+    public float flashSpeed;
+    public Image flashImage;
+    public Color flashColor;
+    PlayerHP playerHP;
+
 	// Use this for initialization
 	void Start () {
-	
+        playerHP = playerHP.GetComponent<PlayerHP>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (playerHP.bDamage)
+        {
+                flashImage.color = flashColor;
+                playerHP.bDamage = false;
+        }
+        else
+        {
+            flashImage.color = Color.Lerp(flashImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
 	}
 }
---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿/*********
+ *		Author:Cameron Asbury 
+ *		Purpose:Attack Script enemies to attack the player
+ ********/
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour {
+
+    GameObject player;
+    public int attack;
+    PlayerHP playerHP;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+        playerHP = player.GetComponent<PlayerHP>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            playerHP.takeDamage(attack);
+        }
+    }
+}
