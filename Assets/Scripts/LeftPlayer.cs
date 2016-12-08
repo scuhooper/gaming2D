744744aa70e128307c475b4e5f4d73e69af6f66a
Cory Brown <yroc.999@gmail.com>
2016-12-07 21:34:22 -0500

    Set up script for two player on one keyboard. Mouse Position for teleporter still only has one input but once this is based on a pick up only one player will be able to use it at a time.
    Just testing things out.

---
+++ Assets/Scripts/LeftPlayer.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftPlayer : MonoBehaviour
+{
+
+	public float speed = 10.0F;
+	//public bool isWalking;
+	public GameObject hitBox;
+	public float Speed = 1f;
+	private float movex = 0f;
+	private float movey = 0f;
+	private Animator animator;
+	public double run = 1.5;
+
+	// variables used for combat and damage. These are accessed in some of the drug scripts.
+	// Questions, ask James
+	public float armor;
+	public int damage;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		hitBox.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		//Movement
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += Vector3.up * Speed * Time.deltaTime;
+			GetComponent<SpriteRenderer>().flipX = true;
+			animator.SetBool("isWalking", true);
+		}
+		else {
+			animator.SetBool("isWalking", false);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += Vector3.down * Speed * Time.deltaTime;
+			animator.SetBool("walkUp", true);
+		}
+		else {
+			animator.SetBool("walkUp", false);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * Speed * Time.deltaTime;
+			animator.SetBool("walkDown", true);
+		}
+		else {
+			animator.SetBool("walkDown", false);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * Speed * Time.deltaTime;
+			animator.SetBool("walkDown", true);
+		}
+		else {
+			animator.SetBool("walkDown", false);
+		}
+
+		//Attack
+		if (Input.GetKey(KeyCode.Q))
+		{
+			hitBox.SetActive(true);
+		}
+		else {
+			hitBox.SetActive(false);
+		}
+
+		//flips the player left and right
+		if (movex <= -0.1f)
+		{
+			// transform.rotation = Quaternion.Euler(0, 180, 0); **Changed by James. Flipping sprite makes using the transform of player in Teleport.cs easier to manage.**
+			GetComponent<SpriteRenderer>().flipX = true;
+		}
+		if (movex >= 0.1f)
+		{
+			// transform.rotation = Quaternion.Euler(0, 0, 0); **Changed by James. Flipping sprite makes using the transform of player in Teleport.cs easier to manage.**
+			GetComponent<SpriteRenderer>().flipX = false;
+		}
+	}
+
+
+}
