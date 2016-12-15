2df5e5129b945897b5623a79d86521ac09c5ba6b
scuhooper <keeling18@marshall.edu>
2016-12-15 17:57:09 -0500

    Made drug spawn script
    Ball also now has force added to it so it bounces around the ground.
    need to add in visual effects to show where some invisible barriers are.
    Set up new colliders in game world to simplify collisions,

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -11,6 +11,7 @@ public class Ball : MonoBehaviour
 	public Transform BallSpawner;
 	public GameObject Goal;
 	public Color lerpedColor;
+	public float kickForce;
 
 
 	void Start()
@@ -32,6 +33,7 @@ public class Ball : MonoBehaviour
 		{
 			God gameScript = NewMethod();
 			gameScript.AddScoreLeft(pointValue);
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 			transform.position = Vector3.Lerp(transform.position, BallSpawner.position, 1);
 			lerpedColor = GetComponent<SpriteRenderer>().color = Color.LerpUnclamped(Color.green, Color.red, Mathf.PingPong(Time.time, 1));
 			transform.parent = BallSpawner;
@@ -46,6 +48,7 @@ public class Ball : MonoBehaviour
 		{
 			God gameScript = NewMethod();
 			gameScript.AddScoreRight(pointValue);
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 			transform.position = Vector3.Lerp(transform.position, BallSpawner.position, 1);
 			lerpedColor = GetComponent<SpriteRenderer>().color = Color.LerpUnclamped(Color.green, Color.red, Mathf.PingPong(Time.time, 1));
 			transform.parent = BallSpawner;
@@ -57,6 +60,16 @@ public class Ball : MonoBehaviour
 		}
 
 
+	}
+
+	private void OnCollisionEnter2D( Collision2D collision )
+	{
+		if ( collision.gameObject.tag == "player" )
+		{
+			Vector2 forceToBall = new Vector2( transform.position.x - collision.gameObject.transform.position.x, transform.position.y - collision.transform.position.y );
+			forceToBall.Normalize();
+			GetComponent<Rigidbody2D>().AddForce( forceToBall * kickForce, ForceMode2D.Impulse );
+		}
 	}
 
 	private static God NewMethod()
